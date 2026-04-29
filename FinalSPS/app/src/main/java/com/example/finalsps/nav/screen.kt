d439cb5ce2733ce14a3e85425f8725d9dbf290da
Fabibi03 <115930539+Fabibi03@users.noreply.github.com>
2026-04-29 18:26:08 +0200

    Add Bookmark screen to navigation

--- FinalSPS/app/src/main/java/com/example/finalsps/nav/screen.kt
+++ FinalSPS/app/src/main/java/com/example/finalsps/nav/screen.kt
@@ -5,5 +5,6 @@ package com.example.finalsps.nav
 sealed class Screen(val route: String) {
     object Map : Screen("map")
     object Search : Screen("search")
+    object Bookmark : Screen("bookmark")
     object Nav : Screen("nav")
 }
