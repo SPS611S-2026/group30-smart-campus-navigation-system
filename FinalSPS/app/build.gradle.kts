fc77b43c7801b1f4f21a6ebd355d91a34f606aa5
Fabibi03 <115930539+Fabibi03@users.noreply.github.com>
2026-04-29 18:32:31 +0200

    Add new dependencies for features
    Added new dependencies for material icons and permissions.

--- FinalSPS/app/build.gradle.kts
+++ FinalSPS/app/build.gradle.kts
@@ -74,6 +74,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // New deps for features
+    implementation("androidx.compose.material:material-icons-extended:1.2.0-alpha02")
+    implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+
     implementation(libs.play.services.maps)
     implementation(libs.play.services.phenotype)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
