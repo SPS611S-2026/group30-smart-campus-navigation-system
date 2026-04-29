05236c3ce99dbfaea27065d90a3b556301bf950a
Fabibi03 <115930539+Fabibi03@users.noreply.github.com>
2026-04-29 18:29:43 +0200

    Add files via upload

---
+++ FinalSPS/app/src/main/java/com/example/finalsps/components/CampusBottomNav.kt
@@ -0,0 +1,86 @@
+package com.example.finalsps.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Navigation
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.finalsps.nav.Screen
+
+@Composable
+fun CampusBottomNav(
+    navController: NavController
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Map, contentDescription = "Map") },
+            label = { Text("Map") },
+            selected = currentRoute == Screen.Map.route,
+            onClick = {
+                navController.navigate(Screen.Map.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Search, contentDescription = "Search") },
+            label = { Text("Search") },
+            selected = currentRoute == Screen.Search.route,
+            onClick = {
+                navController.navigate(Screen.Search.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Bookmark, contentDescription = "Bookmarks") },
+            label = { Text("Bookmarks") },
+            selected = currentRoute == Screen.Bookmark.route,
+            onClick = {
+                navController.navigate(Screen.Bookmark.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Navigation, contentDescription = "Navigate") },
+            label = { Text("Navigate") },
+            selected = currentRoute == Screen.Nav.route,
+            onClick = {
+                navController.navigate(Screen.Nav.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+    }
+}
